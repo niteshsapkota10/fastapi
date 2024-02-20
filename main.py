80e4fad61e5728b9a89f804390b05f9205feb7af
lionitesh10 <lionitesh10@gmail.com>
2024-02-20 16:17:03 +0545

    Password Validation added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from typing import Union
 from fastapi import FastAPI
 from models.user import User
+from pydantic import SecretStr,ValidationError
+import uuid
 
 app=FastAPI()
 users=[]
@@ -10,14 +11,33 @@ async def home():
     return {"message":"Hello World"}
 
 @app.post("/accounts/create/")
-async def create_user(user: User):
-    users.append(user)
+async def signup(user: User):
+    try:
+        user.uuid=uuid.uuid4()
+        users.append(user)
+    except ValidationError as password_error:
+        return {"Error":password_error}
+    
+    print(user)
     return {"User":user}
 
 @app.get("/accounts/lists/")
 async def get_users():
     usernames=[]
     for user in users:
-        usernames.append(user.username)
+        usernames.append(user.uuid)
     return {"Users":usernames}
+
+@app.post("/accounts/login/")
+async def login(username: str,password: SecretStr):
+    for user in users:
+        print()
+        if user.username==username and user.password==password:
+            return {
+                "User Id":user.uuid,
+                "Username":user.username,
+                "Status":"Logged In Successfully"
+            }
+    return {"Status":"Please provide valid credentials!!"}
+
 
--- models/user.py
+++ models/user.py
@@ -1,8 +1,21 @@
-from pydantic import BaseModel
+from typing import Union,Any
+from typing_extensions import Self
+from pydantic import BaseModel,SecretStr,UUID4,field_serializer,model_validator
 
 class User(BaseModel):
     user_id: int
+    uuid: Union[UUID4,None]=None
     username: str
     email: str
-    password: str
+    password: SecretStr
     is_active: bool
+
+    @field_serializer('password',when_used='json')
+    def dump_secret(self,v):
+        return v.get_secret_value()
+
+    @model_validator(mode='after')
+    def validatepassword(self)->Self:
+        if len(self.password)<8:
+            raise ValueError("Password Length Should be Greater than or Equal to 8")
+        return self
